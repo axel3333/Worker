b9da3be38e25af43b6516ab5e572ff8e18e830eb
Alexey Kudryavtsev <axel_47@mail.ru>
2022-02-27 18:23:45 +0300

    Initialize commit

---
+++ Worker.java
@@ -0,0 +1,28 @@
+package Worker;
+
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public Worker(OnTaskErrorListener errorCallback) {
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                continue;
+            } else
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+
+    public void error() {
+        errorCallback.onError("Task " + 33 + " error");
+    }
+}
